2c1f33b87f37c73fd5e78a1da9fdeed9f006f732
Niklas Rosenqvist <niklas.s.rosenqvist@gmail.com>
2026-02-24 20:54:44 +0100

    Add checksums

--- Formula/nitpik.rb
+++ Formula/nitpik.rb
@@ -7,20 +7,20 @@ class Nitpik < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/nsrosenqvist/nitpik/releases/download/v#{version}/nitpik-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER" # updated automatically by CI
+      sha256 "9d6688e5894b53e6bc3153b073d5c11214101f92df848df2ba968650361170ea" # updated automatically by CI
     elsif Hardware::CPU.intel?
       url "https://github.com/nsrosenqvist/nitpik/releases/download/v#{version}/nitpik-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "9d5932b9f595fecd19917d3788be6596da6afec61be07470da850daf4d36c67d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/nsrosenqvist/nitpik/releases/download/v#{version}/nitpik-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "f5a8038363f05a8605cb95c950d264eaa04bf6094597c8183bb7081a15bc771d"
     elsif Hardware::CPU.intel?
       url "https://github.com/nsrosenqvist/nitpik/releases/download/v#{version}/nitpik-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "fd68b8040c3fa0fd08ae68ddf4be950520aa7dc4d70db7f7c32980b2181a7770"
     end
   end
 
